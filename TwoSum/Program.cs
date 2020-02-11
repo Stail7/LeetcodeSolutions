0a199eca99863ebbdee6b1095b8068fb16e63aa7
Oleksandr Parshakov <stail.7ff@gmail.com>
2020-02-11 19:08:23 +0200

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 2, 5, 5, 11 };
+            var target = 10;
+            foreach (var num in TwoSum(nums, target))
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+            throw new ArgumentException("No two sum solution");
+        }
+    }
+}
